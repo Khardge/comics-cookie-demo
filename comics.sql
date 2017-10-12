b437d95eae85849f88c5aff2d8382fceab948e21
Blake Mills <welzie@gmail.com>
2017-10-12 15:27:12 -0500

    just getting started

---
+++ comics.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Oct 12, 2017 at 09:40 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `comics`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comic`
+--
+
+CREATE TABLE `comic` (
+  `id` int(11) NOT NULL,
+  `title` varchar(120) NOT NULL,
+  `issue_number` int(11) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `publisher_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comic`
+--
+
+INSERT INTO `comic` (`id`, `title`, `issue_number`, `year`, `publisher_id`) VALUES
+(4, 'The Amazing Spider-man', 1, '1963', 1),
+(9, 'The Amazing Spider-man', 2, '1963', 1),
+(10, 'The Amazing Spider-man', 3, '1963', 1),
+(11, 'The Amazing Spider-man', 4, '1963', 1),
+(12, 'The Amazing Spider-man', 5, '1963', 1),
+(13, 'Batman', 1, '1940', 2),
+(14, 'Batman', 2, '1940', 2),
+(15, 'Batman', 3, '1940', 2),
+(16, 'Batman', 4, '1940', 2),
+(17, 'Wonder Woman', 1, '1942', 2),
+(18, 'Wonder Woman', 2, '1942', 2),
+(19, 'Wonder Woman', 3, '1942', 2),
+(20, 'Wonder Woman', 4, '1942', 2),
+(21, 'Wolverine', 1, '1982', 1),
+(22, 'X-Men', 1, '1963', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `id` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `publisher`
+--
+
+INSERT INTO `publisher` (`id`, `name`) VALUES
+(1, 'Marvel'),
+(2, 'DC');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comic`
+--
+ALTER TABLE `comic`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `publisher_id` (`publisher_id`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comic`
+--
+ALTER TABLE `comic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `publisher`
+--
+ALTER TABLE `publisher`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comic`
+--
+ALTER TABLE `comic`
+  ADD CONSTRAINT `comic_ibfk_1` FOREIGN KEY (`publisher_id`) REFERENCES `publisher` (`id`);
---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://comics:12345678@localhost:8889/comics'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+
+
+class Comic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    issue_number = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.String(4), nullable=False)
+    publisher_id = db.Column(db.Integer, ForeignKey("publisher.id"))
+
+    def __init__(self, title, issue_number, year, publisher_id):
+        self.title = title
+        self.year = year
+        self.issue_number = issue_number
+        self.publisher_id = publisher_id
+
+    def __repr__(self):
+        return '<Comic {title}, {year}>'.format(title=self.title, year=self.year)
+
+class Publisher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Publisher {}>'.format(self.name)
+
+@app.route("/")
+def new_post():
+    comics = Comic.query.all()
+    return render_template('index.html', comics=comics)
+
+if __name__ == "__main__":
+    app.run()
